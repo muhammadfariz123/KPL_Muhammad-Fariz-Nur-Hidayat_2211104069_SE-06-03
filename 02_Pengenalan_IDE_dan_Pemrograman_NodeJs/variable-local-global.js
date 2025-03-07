905632dd105bd45a16c47a98a8dee25ba8240267
ekapurnia16 <ekapurnia16@gmail.com>
2025-03-07 13:01:23 +0700

    First Commit

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Muhammad Fariz Nur Hidayat"
+
+function getMyIdentity() {
+    let localVariable = 2211104069;
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,15 @@
+var name = "Muhammad Fariz Nur Hidayat";
+const nim = 2211104069;
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim);
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`);
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`);
+
+
+var name = "ayam"
+
+let fullName = "Muhammad Fariz Nur Hidayat"
