c1af681feba95b56754055f865c50ce01ae27b0d
muhammadfariz123 <warungkombas@gmail.com>
2025-03-28 09:34:44 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_Generics/TP/tpModul5.js
@@ -0,0 +1,22 @@
+// 4. MENAMBAHKAN METHOD DENGAN GENERIC
+class HaloGeneric {
+    static SapaUser(user) {
+        console.log(`Halo user ${user}`);
+    }
+}
+
+HaloGeneric.SapaUser("Muhammad Fariz Nur Hidayat");
+
+// 6. MENAMBAHKAN CLASS DENGAN GENERIC
+class DataGeneric {
+    constructor(data) {
+        this.data = data;
+    }
+
+    PrintData() {
+        console.log(`Data yang tersimpan adalah: ${this.data}`);
+    }
+}
+
+const data = new DataGeneric("2211104069"); 
+data.PrintData();
